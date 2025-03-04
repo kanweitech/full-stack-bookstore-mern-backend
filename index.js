09552d458d48722004e5a29345c79da99797b6cc
kanweitech <68553986+kanweitech@users.noreply.github.com>
2025-03-04 04:13:54 -0800

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.use(cors({
 
 // routes
 const bookRoutes = require('./src/books/book.route');
-const orderRoutes = require("./src/orders/order.route")
-const userRoutes =  require("./src/users/user.route")
-const adminRoutes = require("./src/stats/admin.stats")
+const orderRoutes = require("./src/orders/order.route");
+const userRoutes =  require("./src/users/user.route");
+const adminRoutes = require("./src/stats/admin.stats");
 
 app.use("/api/books", bookRoutes)
 app.use("/api/orders", orderRoutes)
